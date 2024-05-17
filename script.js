21cdb50412e37a86f6c33a0ed38b9b1674e85508
Marcos <marcospiccotto12@gmail.com>
2024-05-17 08:41:58 -0300

    Correcion

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let datos = [2, 3, 4];
 let aux = 0;
 
-//Sumo todos los elems pares
+//Sumo todos los elems impares
 for (let i = 0; i < datos.length; i++) {
-    if (datos[i] % 2 === 0) {
+    if (datos[i] % 2 === 1) {
         aux += datos[i];
     }
 }
